42f1bf01b9cae740fe6aa08aadab2094b625086b
MaheshUdvag <maheshudvag@gmail.com>
2022-02-16 00:08:19 +0530

    added cache time

--- js/cache.js
+++ js/cache.js
@@ -2,19 +2,27 @@ const cache = await caches.open("anime-v1");
 const cacheAvailable = "caches" in self;
 
 export const getDataFromCache = async (key) => {
-  let data;
+  let cachedData;
   if (cacheAvailable) {
     const response = await cache.match(key);
 
     if (response) {
-      data = await response.json();
+      const { data, cachedTime } = await response.json();
+      const diffDays = parseInt(
+        (new Date().getTime() - cachedTime) / (1000 * 60 * 60 * 24)
+      );
+      if (diffDays < 2) {
+        cachedData = data;
+      }
     }
   }
 
-  return data;
+  return cachedData;
 };
 
 export const setDataToCache = async (key, data) => {
+  const cachedTime = new Date().getTime();
+  data = { cachedTime, data };
   if (cacheAvailable) await cache.put(key, new Response(JSON.stringify(data)));
 };
 
